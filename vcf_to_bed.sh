8dce16f12c53cb9dd0c694f50a7ad664279935f6
Laure Fresard <lfresard@DN52eo2f.SUNet>
2019-05-24 07:44:23 -0700

    converst vcf to bed file

---
+++ vcf_to_bed.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#LF
+# May 2019
+# Transform gnomad VCF files to bed format
+
+	# Put multiallelic lines separatly
+	# Extract variant sites and allelic info 
+	# Transform in bed format
+vcf_file=$1
+bed_file="${vcf_file%.vcf.gz}.bed"
+
+echo "processing $vcf_file"
+bcftools norm -m "-both" ${vcf_file} | bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t%AF\n' | awk '{OFS="\t"; print "chr"$1, $2-1, $2, $3, $4, $5, $6, $7}' |sort -k1,1 -k2,2n > $bed_file
+
+
+echo "done with $vcf_file"
+
